5e9d5f04960a8b886131ebe899f93f6253e1d8c4
Stefan Bauer <stefan.bauer@n8d.at>
2020-06-05 01:26:28 +0200

    after delete

---
+++ tasks/src/tasks/longlast.server.js
@@ -0,0 +1,17 @@
+const browserSync = require('browser-sync');
+
+const server = (config) => {
+
+    // Create new instance of browser-sync
+    const bsServer = browserSync.create();
+
+    bsServer.init(config);
+
+    return bsServer;
+
+};
+
+module.exports = {
+    init: server,
+    reload: browserSync.reload
+};
